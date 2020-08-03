b9dfe291dce2da98abd73a41aaae16d5c61ddc8d
sangeetha <Sangeetha@DESKTOP-BT8EDFF>
2020-08-03 21:44:03 +0530

    3rd commit

---
+++ src/SubOfNum.java
@@ -0,0 +1,12 @@
+
+public class SubOfNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8,b=4,sub=0;
+		sub=a-b;
+		System.out.println(sub);
+
+	}
+
+}
